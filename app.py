cdc773678f4a8f8757e2b88e1daa2deed42c4ee3
Zloiben <daniil2019.milkork@yandex.ru>
2022-02-26 17:49:09 +0800

    Мелкие изменение

--- app.py
+++ app.py
@@ -20,29 +20,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Data.sqlite3'
 db = SQLAlchemy(app)
 
-
-class Users(db.Model):
-
-    id = db.Column('id', db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    password = db.Column(db.String(50))
-    role = db.Column(db.String(20))
-    email = db.Column(db.String(100))
-    data = db.Column(db.TEXT())
-
-    def __init__(self, name, role, password, email, data):
-
-        self.name = name
-        self.password = password
-        self.role = role
-        self.email = email
-        self.data = data
-
-    def __repr__(self):
-        return '<User %r>' % self.name
-
-
-db.create_all()
+from models import Users
 
 
 @app.route('/')
---
+++ models.py
@@ -0,0 +1,25 @@
+from app import db
+
+
+class Users(db.Model):
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(50))
+    role = db.Column(db.String(20))
+    email = db.Column(db.String(100))
+    data = db.Column(db.TEXT())
+
+    def __init__(self, name, role, password, email, data):
+
+        self.name = name
+        self.password = password
+        self.role = role
+        self.email = email
+        self.data = data
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+db.create_all()
