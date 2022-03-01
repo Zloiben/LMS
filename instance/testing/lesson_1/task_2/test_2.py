dfd8373b1c28b8b45cb619ebe47a7aa636632cdc
Zloiben <daniil2019.milkork@yandex.ru>
2022-03-01 17:13:45 +0800

    Передел тест для 2 задачи Так же сделал заготовку на более потребные ошибки

--- instance/testing/lesson_1/task_2/test_2.py
+++ instance/testing/lesson_1/task_2/test_2.py
@@ -1,29 +1,35 @@
 from subprocess import Popen, PIPE
-COMMAND = r"python ./instance/testing/lesson_1/task_2/test_file_2.py"
+
+COMMAND = r"python ./test_file_2.py"
+
+# input, result
+test_variants = [
+    ("3\n2\n1", "1\n2\n3"),
+    ("Hi\nCarramba!\nHohoho", "Hohoho\nCarramba!\nHi"),
+    ("Карамба!\nКоррида!\nЧерт подери!", "Черт подери!\nКоррида!\nКарамба!")
+]
 
 
-def test_1(input_data, result):
+def testing(input_data: str, result_waiting: str) -> tuple:
+    """
+    функция для тестирования отправленных файлов.
+    :param input_data: вводимые данные.
+    :param result_waiting: ожидаемый результат.
+    :return Возращает кортеж (Результат, что было выведенною)
+    """
     p = Popen(COMMAND, stdout=PIPE, stdin=PIPE, encoding='utf-8')
     out, err = p.communicate(input=input_data)
-    return out.strip() == result
-
-
-def test_2(input_data, result):
-    p = Popen(COMMAND, stdout=PIPE, stdin=PIPE, encoding='utf-8')
-    out, err = p.communicate(input=input_data)
-    return out.strip() == result
-
-
-def test_3(input_data, result):
-    p = Popen(COMMAND, stdout=PIPE, stdin=PIPE, encoding='utf-8')
-    out, err = p.communicate(input=input_data)
-    return out.strip() == result
+    return out.strip() == result_waiting, out.strip()
 
 
 def get_result():
     """Ответ Hello, World!"""
-    if test_1("3\n2\n1", "1\n2\n3"):
-        if test_2("Hi\nCarramba!\nHohoho", "Hohoho\nCarramba!\nHi"):
-            if test_3("Карамба!\nКоррида!\nЧерт подери!", "Черт подери!\nКоррида!\nКарамба!"):
-                return True
+    data_testing = []
+    for index, test in enumerate(test_variants):
+        result_testing = testing(test[0], test[1])
+        data_testing.append(result_testing[0])
+        # TODO: Пока что сдеалл чтобы просто возращало True или False
+        #   В будущем trello Переделать тесты
+    if all(data_testing):
+        return True
     return False
